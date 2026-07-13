0a31166ea87e60910850f517043600522d13c2b0
madewarabhishek966529-wq <madewarabhishek966529@gmail.com>
2026-07-13 20:45:35 +0530

    6_mid_of_two_array_sorted

---
+++ 6_mid_of_two_array_sorted.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        result = nums1 + nums2
+        result.sort()
+
+        n = len(result)
+        mid = n // 2
+
+        if n % 2 == 1:      # Odd number of elements
+            return result[mid]
+        else:               # Even number of elements
+            return (result[mid - 1] + result[mid]) / 2.0
