b954e4e1fa9d314176eb1c1b9898a1190a4e967c
Abhishek Madewar <madewarabhishek966529@gmail.com>
2026-07-22 21:01:06 +0530

    Update 6_mid_of_two_array_sorted.py

--- 6_mid_of_two_array_sorted.py
+++ 6_mid_of_two_array_sorted.py
@@ -6,7 +6,7 @@ class Solution(object):
         n = len(result)
         mid = n // 2
 
-        if n % 2 == 1:      # Odd number of elements
+        if n % 2 == 1:     
             return result[mid]
         else:               # Even number of elements
             return (result[mid - 1] + result[mid]) / 2.0
